912f1d504aa198e3bba33742ff55b6080d5a9410
Abhijith_C_N <abhijithcn28@gmail.com>
2025-03-20 00:15:36 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,5 +1,5 @@
 variable "ssh_key_name" {
   description = "The name of the SSH key pair to use for instances"
   type        = string
-  default     = "my_universal_key"
+  default     = "my_universal_key1"
 }
